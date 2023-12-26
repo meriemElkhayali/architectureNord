e4791c5ee38fe2eae30fc9863816b632a3bb9089
meriemElkhayali <meriem.elkhayali@gmail.com>
2023-12-26 17:11:22 +0100

    change img1 first3

--- src/composers/Plant.js
+++ src/composers/Plant.js
@@ -4,7 +4,7 @@ export default function Plant() {
   return (
     <div>
      <section class="component">
-     <img src='image/Capture_d_écran_2023-11-26_132316-removebg-preview.png' style={{ width: "50px" }} />
+     <img src='https://firebasestorage.googleapis.com/v0/b/archivideo-14dff.appspot.com/o/logoarchit.png?alt=media&token=7e2b7b54-9981-4a84-99c0-e9ab02ae95c3' style={{ width: "50px" }} />
 
 <blockquote class="callout quote EN">
 Bienvenue dans l'univers de <span style={{ color: "#e42f2f"}}>STE ABDO ARCHI NORD SARL </span>- où chaque projet est une aventure architecturale unique, et chaque client est au cœur de notre inspiration.<br/>
